889299d668e1de9a2b4b7ecdfc0a2a91278bd3ea
Ashok Sahara <ashoksahara@Ashoks-MacBook-Pro.local>
2017-08-22 15:16:28 +0530

    sm

--- src/main/java/com/clickpost/client/service/ordercreation/Test.java
+++
@@ -1,8 +0,0 @@
-package com.clickpost.client.service.ordercreation;
-
-import com.clickpost.client.service.ordercreation.OrderCreationServiceImpl;
-public class Test {
-    public static void main(String[] args) {
-        OrderCreationServiceImpl orderCreationService = new OrderCreationServiceImpl();
-    }
-}
